e8af6866e972ff6f4a62c70671046e9b34b3d36e
mujeebkhan <khanmujeeb687@gmail.com>
2026-04-16 01:10:38 +0530

    nit

--- app/src/main/java/com/mintech/parkwiseapp/ui/screens/VehicleSetupScreen.kt
+++ app/src/main/java/com/mintech/parkwiseapp/ui/screens/VehicleSetupScreen.kt
@@ -1,5 +1,6 @@
 package com.mintech.parkwiseapp.ui.screens
 
+import androidx.compose.foundation.border
 import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.background
